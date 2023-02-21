bc006458c4d97406a359d247fbdc48106a7d821f
ogaroh <ogaroerick1998@gmail.com>
2023-02-21 01:48:42 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - Check description
- * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Description: It prints the alphabet followed by a new line
  * Return: Nothing.
  */
 void print_alphabet(void)
