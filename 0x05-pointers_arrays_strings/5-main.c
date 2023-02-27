4058fe0d6115e9333849be7373b794ef60d7c96d
ogaroh <ogaroerick1998@gmail.com>
2023-02-27 13:52:44 +0300

    5-rev_string

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-  char s[10] = "Holberton";
+  char s[10] = "My School";
 
   printf("%s\n", s);
   rev_string(s);
