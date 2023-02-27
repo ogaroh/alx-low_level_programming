7e51c8a283f76844a2d7f8c38fef3f9b07cb75c3
ogaroh <ogaroerick1998@gmail.com>
2023-02-27 13:55:18 +0300

    7-puts_half

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -9,7 +9,7 @@ int main(void)
 {
   char *str;
 
-  str = "0123456780";
+  str = "0123456789";
   puts_half(str);
   return (0);
 }
