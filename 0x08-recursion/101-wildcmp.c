1683560f12899e7e5f16a502432fca494469c2ad
ogaroh <ogaroerick1998@gmail.com>
2023-03-08 17:43:11 +0300

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,15 +5,44 @@
  * @s2: Second input string
  * Return: 1 if strings identical and 0 in otherwise
  */
+
+int find_other(char s, char *str, int *counter)
+{
+	if (s == *str)
+		return (1);
+	else if (*str == '\0')
+		return (0);
+	else if (s == '*')
+		return (0);
+	else
+	{
+		*counter += 1;
+		return (find_other(s, ++str, &*counter));
+	}
+}
+
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1 && !*s2)
+	int counter;
+
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
+	else if (*s1 == *s2)
+		return (wildcmp(++s1, ++s2));
+	else if (*s1 == '*')
+	{
+		counter = 0;
+		find_other(*++s1, s2, &counter);
+		s2 += counter;
+		return (wildcmp(s1++, s2++));
+	}
+	else if (*s2 == '*')
+	{
+		counter = 0;
+		find_other(*++s2, s1, &counter);
+		s1 += counter;
+		return (wildcmp(s1++, s2++));
+	}
+	else
 		return (0);
-	return (0);
 }
